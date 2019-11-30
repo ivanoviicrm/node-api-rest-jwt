d4b5f5a5f53ab0f6de387b7ca03555dc215320f2
Iván <ivanoviicrm@gmail.com>
2019-11-30 21:08:08 +0100

    irm: Added a minLength to user names

--- src/validations/auth.js
+++ src/validations/auth.js
@@ -8,7 +8,7 @@ const loginValidationsSchema = Joi.object({
 
 // Schema used to validate User Register
 const registerValidationsSchema = Joi.object({
-  name: Joi.string().max(50).required(),
+  name: Joi.string().min(3).max(50).required(),
   email: Joi.string().email().required(),
   password: Joi.string().min(8).required()
 });
