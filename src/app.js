67c7395d45a0debea946179cd6db6fb8e614e0b9
Iván <ivanoviicrm@gmail.com>
2019-11-30 12:24:32 +0100

    irm: Added first logic to connect to db and installed bcryptjs

--- src/app.js
+++ src/app.js
@@ -1,8 +1,18 @@
 const dotenv = require('dotenv').config();
+const bcrypt = require('bcryptjs');
+const mongoose = require('mongoose');
 const express = require('express');
 const app = express();
 const registerRoute = require('./routes/auth/register');
 const loginRoute = require('./routes/auth/login');
+
+// MONGO DB CONNECTION
+mongoose.connect(`mongodb://localhost/${process.env.DB_NAME}`, {useUnifiedTopology: true, useNewUrlParser: true});
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log(`Connected to DB -> mongodb://localhost/${process.env.DB_NAME}`);
+});
 
 // MIDDLEWARES
 app.use(express.json());
--- src/routes/auth/register.js
+++ src/routes/auth/register.js
@@ -1,8 +1,8 @@
 const routes = require('express').Router();
 
 // URL -> http://localhost:PORT/api/v1/register
-routes.get('/register', (req, res) => {
-  res.status(200).json({ message: 'Connected!' });
+routes.post('/register', (req, res) => {
+  console.log(req.body);
 });
 
 module.exports = routes;
