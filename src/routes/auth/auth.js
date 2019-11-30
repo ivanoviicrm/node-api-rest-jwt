64b8b4032572dbb9215696cdc5c846820baed483
Iván <ivanoviicrm@gmail.com>
2019-11-30 21:32:51 +0100

    irm: Feature UserRegistration finished

--- src/routes/auth/auth.js
+++ src/routes/auth/auth.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 const userModel = require('../../models/User');
 const Joi = require('@hapi/joi');
 const { registerValidationsSchema, loginValidationsSchema } = require('../../validations/auth');
+const bcrypt = require('bcryptjs');
 
 // URL -> http://localhost:PORT/api/v1/register
 router.post('/register', async (req, res) => {
@@ -18,26 +19,39 @@ router.post('/register', async (req, res) => {
       if (emailAlreadyExist) {
         return res.status(400).send('User already exists!');
       }
-    } catch (error) {
-        return res.status(400).send(error);
+    } catch (error) { // ERROR 001
+      console.log('IRM(001) -> An internal error has occurred while checking if a user already exists when someone was trying to register.', error);
+      return res.status(400).send('An internal error has occurred, please try again later (001)');
     }
 
-    // Creating db model
-    const user = new userModel({
-      name: req.body.name,
-      email: req.body.email,
-      password: req.body.password
-    });
-
-    // Trying to save the user
     try {
-      const savedUser = await user.save();
-      res.status(200).send('User saved!');
-    } catch(error) {
-      res.status(400).send(error);
-    }
+      // Encrypting the password before its storation
+      const salt = await bcrypt.genSalt(10);
+      const encryptedPassword = await bcrypt.hash(req.body.password, salt);
 
-  });
+      // Creating db model
+      const user = new userModel({
+        name: req.body.name,
+        email: req.body.email,
+        password: encryptedPassword
+      });
+
+      // Trying to save the user
+      try {
+        const savedUser = await user.save();
+        res.status(200).send('User saved!');
+
+      } catch(error) { // ERROR 003
+        console.log('IRM(003) -> An internal error has occurred while trying to store new user data when someone was trying to register.', error);
+      return res.status(400).send('An internal error has occurred, please try again later (003)');
+      }
+      
+    } catch (error) { // ERROR 002
+      console.log('IRM(002) -> An internal error has occurred while attempting to encrypt a password when someone was trying to register.', error);
+      return res.status(400).send('An internal error has occurred, please try again later (002)');
+    }
+});
+ 
 
 // URL -> http://localhost:PORT/api/v1/login
 router.get('/login', (req, res) => {
